e1588b058631cef7aa343b5d270dee2f0ec8d754
Sprite <SpriteOvO@gmail.com>
2023-01-23 22:54:11 +0800

    Write out logs to files

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
 use core::config;
-use std::{env, process::exit};
+use std::{env, fs, path::PathBuf, process::exit, sync::Arc};
 
 use anyhow::anyhow;
-use spdlog::prelude::*;
+use const_format::formatcp;
+use spdlog::{
+    prelude::*,
+    sink::{RotatingFileSink, RotationPolicy},
+};
 
 #[tokio::main]
 async fn main() {
-    setup_logger();
+    let log_dir = setup_logger();
 
     info!("{} startup!", config::PACKAGE.name);
     info!("current version: {}", config::PACKAGE.version);
+    info!("logs will be written to '{}'", log_dir.display());
 
     if let Err(err) = run().await {
         error!("exited with err: {err}");
@@ -17,10 +22,35 @@ async fn main() {
     }
 }
 
-fn setup_logger() {
+fn setup_logger() -> PathBuf {
     if cfg!(debug_assertions) {
         spdlog::default_logger().set_level_filter(LevelFilter::All)
     }
+
+    let log_dir = dirs::home_dir()
+        .expect("no home directory found")
+        .join(formatcp!(".{}/logs", config::PACKAGE.name));
+
+    fs::create_dir_all(&log_dir).expect("failed to create log directory");
+
+    let file_sink = Arc::new(
+        RotatingFileSink::builder()
+            .base_path(log_dir.join("log.txt"))
+            .rotation_policy(RotationPolicy::Daily { hour: 0, minute: 0 })
+            .build()
+            .expect("failed to build log file sink"),
+    );
+
+    let logger = spdlog::default_logger()
+        .fork_with(|logger| {
+            logger.sinks_mut().push(file_sink);
+            Ok(())
+        })
+        .expect("failed to build logger");
+
+    spdlog::set_default_logger(logger);
+
+    log_dir
 }
 
 async fn run() -> anyhow::Result<()> {
